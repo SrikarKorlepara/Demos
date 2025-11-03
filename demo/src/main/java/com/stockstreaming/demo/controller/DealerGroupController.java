8cf25a7cbd12b73bb618eb52d0c29db8346785ba
Srikar Korlepara <srikar.k.v.20@gmail.com>
2025-11-04 01:35:36 +0530

    Commented out stocks model

--- demo/src/main/java/com/stockstreaming/demo/controller/DealerGroupController.java
+++ demo/src/main/java/com/stockstreaming/demo/controller/DealerGroupController.java
@@ -1,12 +1,12 @@
 package com.stockstreaming.demo.controller;
 
+import com.stockstreaming.demo.dto.DealerGroupCreateRequestDto;
+import com.stockstreaming.demo.dto.DealerGroupRequestDto;
 import com.stockstreaming.demo.dto.DealerGroupResponseDto;
 import com.stockstreaming.demo.service.DealerGroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -17,9 +17,35 @@ public class DealerGroupController {
     private final DealerGroupService dealerGroupService;
 
     @GetMapping("/{businessId}")
-    public Mono<ResponseEntity<DealerGroupResponseDto>> getDealerGroupByBusinessId(String businessId) {
+    public Mono<ResponseEntity<DealerGroupResponseDto>> getDealerGroupByBusinessId(@PathVariable String businessId) {
         DealerGroupResponseDto dealerGroup = dealerGroupService.getDealerGroupByBusinessId(businessId);
         return Mono.just(ResponseEntity.ok(dealerGroup));
     }
 
+    @PostMapping
+    public Mono<ResponseEntity<DealerGroupResponseDto>> createDealerGroup(@RequestBody Mono<DealerGroupCreateRequestDto> dealerGroupRequestDtoMono) {
+        return dealerGroupRequestDtoMono
+                .map(dealerGroupService::createDealerGroup)
+                .map(ResponseEntity::ok);
+    }
+
+    @PutMapping("/{businessId}")
+    public Mono<ResponseEntity<DealerGroupResponseDto>> updateDealerGroup(
+            @PathVariable String businessId,
+            @RequestBody Mono<DealerGroupRequestDto> dealerGroupRequestDtoMono
+    ) {
+        return dealerGroupRequestDtoMono
+                .map(dto -> dealerGroupService.updateDealerGroup(businessId,dto))
+                .map(ResponseEntity::ok);
+    }
+
+    @PatchMapping("/{businessId}")
+    public Mono<ResponseEntity<DealerGroupResponseDto>> partialUpdateDealerGroup(
+            @PathVariable("businessId") String businessId,
+            @RequestBody Mono<DealerGroupRequestDto> dealerGroupRequestDtoMono
+    ) {
+        return dealerGroupRequestDtoMono
+                .map(dto -> dealerGroupService.partialUpdateDealerGroup(businessId,dto))
+                .map(ResponseEntity::ok);
+    }
 }
--- demo/src/main/java/com/stockstreaming/demo/model/Stock.java
+++ demo/src/main/java/com/stockstreaming/demo/model/Stock.java
@@ -1,19 +1,19 @@
-package com.stockstreaming.demo.model;
-
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "stocks")
-@Data
-public class Stock {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String symbol;
-    private String price;
-    private String sector;
-    private String industry;
-}
+//package com.stockstreaming.demo.model;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Entity
+//@Table(name = "stocks")
+//@Data
+//public class Stock {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    private String symbol;
+//    private String price;
+//    private String sector;
+//    private String industry;
+//}
